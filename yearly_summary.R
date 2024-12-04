834f0263f493ea45fadc647503a4e7ca86292c35
rfoxall3 <103981140+rfoxall3@users.noreply.github.com>
2024-12-04 17:26:30 -0500

    added plots
    This update adds graphics capability for a key plot (Cleveland dot plot) and the data formatting required to make the plot.

--- yearly_summary.R
+++ yearly_summary.R
@@ -35,10 +35,11 @@ cleaner_table <- source_table %>%
   group_by(service_rte_num, weekday) %>%
   mutate(yrly_rel = mean(reliability)) %>% ungroup()
 
-# now we want the top 10 least reliable (latest) routes for the year
+# now we want the top 10 least reliable (latest) routes for the year in order
 reliability_list <- cleaner_table %>%
   group_by(service_rte_num, weekday) %>%
-  summarize(yrly_rel = mean(reliability))
+  summarize(yrly_rel = mean(reliability)) %>%
+  arrange(yrly_rel)
 
 write_csv(reliability_list, "reliability_list.csv")
 
@@ -53,3 +54,28 @@ ggplot(table1, aes(x = week_start, y = reliability2, color = weekday)) +
   labs(title = "Reliability Over Time", x = "Week Of", y = "Average Lateness (seconds)") +
   scale_color_manual(values = c("weekday" = "blue", "weekend" = "red")) +
   geom_hline(aes(yintercept = yrly_rel, color = weekday), linetype = "dashed")
+
+# the above is an exploratory graph; now we need to make a more useful descriptive map for analysis
+# top ten late bus routes showing weekend and weekday reliability
+# removes routes without weekend service
+table2 <- reliability_list %>% pivot_wider(names_from = weekday, values_from = yrly_rel) %>% 
+  rename(route = service_rte_num, weekday_lateness = weekday, weekend_lateness = weekend) %>% na.omit
+
+# listing top ten latest routes (that have both weekday and weekend service)
+# also converting lateness into minutes for the sake of our graph being easier to understand
+late10 <- table2 %>% filter(route %in% c(11, 208, 271, 8, 255, 62, 28, 43, 5, 132)) %>%
+  mutate(weekday_lateness = weekday_lateness / 60, weekend_lateness = weekend_lateness / 60) %>%
+  mutate(route=factor(route))
+
+# making a nice cleveland dot plot to show weekend and weekday lateness!
+# foundational code via r graph gallery (https://r-graph-gallery.com/303-lollipop-plot-with-2-values.html)
+ggplot(late10) +
+  geom_segment( aes(x=route, xend=route, y=weekday_lateness, yend=weekend_lateness), color="grey") +
+  geom_point( aes(x=route, y=weekday_lateness), color=rgb(0.2,0.7,0.1,0.5), size=3 ) +
+  geom_point( aes(x=route, y=weekend_lateness), color=rgb(0.7,0.2,0.1,0.5), size=3 ) +
+  coord_flip()+
+  theme(
+    legend.position = "top",
+  ) +
+  xlab("Route Number") +
+  ylab("Lateness (minutes past scheduled time)")
