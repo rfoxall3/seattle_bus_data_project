1c3159ebce25ab0d6ff96de9c7398c3f1fc27e29
rfoxall3 <103981140+rfoxall3@users.noreply.github.com>
2024-12-09 11:36:18 -0500

    adds stop location info
    Makes stop_summary a better mirror of seattle_routes by providing information about the stop (cross-streets, bus shelters, etc.) and generates lists of the least reliability bus stops in KC and in Seattle proper.

--- stop_summary.R
+++ stop_summary.R
@@ -50,3 +50,39 @@ reliability_list_weekday <- cleaner_table %>%
   arrange(desc(week_rel))
 
 write_csv(reliability_list_weekday, "reliability_weekday_by_stop.csv")
+
+# equivalent of seattle_routes for each stop -- get info about the stop by stop ID
+# stop info provided via King County Metro transit stops spatial dataset
+# https://gis-kingcounty.opendata.arcgis.com/datasets/kingcounty::transit-stops-for-king-county-metro-transitstop-point/about
+stop_info <- read_csv("transit_stops_kcm.csv") %>% clean_names()
+
+# only selecting info we're interested in (some kept here will be removed later)
+stop_info <- stop_info %>% 
+  select(stop_id, auth_code, auth_name, bearing_code, cf_cross_streetname, hastus_cross_street_name, in_service_flag,
+         jurisdiction, kcm_managed_equipment, route_list, on_street_name, num_shelters)
+
+# only want stops that are in service and have routes listed
+stop_info <- stop_info %>%
+  filter(in_service_flag == "Y") %>%
+  filter(is.na(route_list) == F)
+
+# join the stop info to the reliability list
+# left join prioritizes stop info dataset (so we can see what's missing from the reliability data)
+test <- left_join(stop_info, reliability_list_overall, by="stop_id")
+
+# slightly more cleaning here -- first turning reliability into minutes rather than seconds
+# also making a single variable called "stop name" that gives the cross-street info
+stop_reliability <- test %>%
+  mutate(yrly_rel_mins = yrly_rel/60) %>%
+  arrange(desc(yrly_rel_mins)) %>%
+  select(-hastus_cross_street_name, -in_service_flag, -auth_code) %>%
+  mutate(stop_name = paste0(on_street_name, " & ", cf_cross_streetname)) %>%
+  select(-cf_cross_streetname, -on_street_name)
+
+write_csv(stop_reliability, "stop_reliability.csv")
+
+# limited to stops in the city of Seattle
+stop_reliability_seattle <- stop_reliability %>%
+  filter(jurisdiction == "SEA")
+
+write_csv(stop_reliability_seattle, "stop_reliability_seattle.csv")
